16165cd6d7ae66cdb27b9330ec3cddcf6d167f9a
Evan <evan2016@vip.qq.com>
2016-12-12 11:35:55 +0800

    更新

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,7 +52,7 @@ $(function() {
     var onOff = true;
     var skillProgess = function(onOff) {
         if (onOff) {
-            var paArr = [95, 95, 85, 80, 90, 60];
+            var paArr = [95, 95, 85, 85, 80, 60];
             $.progressBar('drawRing', paArr);
         }
     }
